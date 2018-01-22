09c971103c420d5c562f7b523c83ee92051fee77
Dave Kornbau <david.kornbau@gmail.com>
2018-01-22 15:53:47 -0500

    added new roles

--- src/main/java/com/wafflemkr/fusc/security/AuthoritiesConstants.java
+++ src/main/java/com/wafflemkr/fusc/security/AuthoritiesConstants.java
@@ -11,6 +11,24 @@ public final class AuthoritiesConstants {
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
+    public static final String COACH = "ROLE_COACH";
+
+    public static final String ASSISTANT_COACH = "ROLE_ASSISTANT_COACH";
+
+    public static final String MANAGER = "ROLE_MANAGER";
+
+    public static final String REFEREE = "ROLE_REFEREE";
+
+    public static final String REFEREE_DIRECTOR = "ROLE_REFEREE_DIRECTOR";
+
+    public static final String TOURNAMENT_DIRECTOR = "ROLE_TOURNAMENT_DIRECTOR";
+
+    public static final String VOLUNTEER = "ROLE_VOLUNTEER";
+
+    public static final String VOLUNTEER_DIRECTOR = "ROLE_VOLUNTEER_DIRECTOR";
+
+    public static final String CLINIC_DIRECTOR = "ROLE_CLINIC_DIRECTOR";
+
     private AuthoritiesConstants() {
     }
 }
